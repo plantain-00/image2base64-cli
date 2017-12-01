210dd6533a4ecd439dc5f4751c9db80c08881170
york yao <yaoao12306@outlook.com>
2017-12-01 16:13:44 +0800

    fix: remove unused script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -28,7 +28,6 @@ module.exports = {
     ts: `tslint --fix ${tsFiles}`,
     js: `standard --fix ${jsFiles}`
   },
-  release: `clean-release`,
   watch: {
     ts: `${tscSrcCommand} --watch`,
     demo: `${demoCommand} --watch`
